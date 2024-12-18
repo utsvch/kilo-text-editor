b71f31c33a057b17b964854bf4068fc0629d546c
Utsav Chatterjee <c.utsav1@gmail.com>
2024-12-18 21:40:19 +0530

    Welcome message

--- kilo.c
+++ kilo.c
@@ -13,6 +13,10 @@ struct termios orig_termios;
 #define FLAG_OUTPUT 2
 #define FLAG_CONTROL 4
 #define FLAG_LOCAL 8
+#define CURSOR_SHOW "\x1b[?25l" // hide cursor
+#define CURSOR_HIDE "\x1b[?25h" // show cursor
+
+#define KILO_VERSION "0.0.1"
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -54,8 +58,22 @@ struct editorConfig E;
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
 
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -66,12 +84,14 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
 
-  abAppend(&ab, "\x1b[2J", 4);
+  abAppend(&ab, CURSOR_HIDE, 6);
+//  abAppend(&ab, "\x1b[2J", 4);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, CURSOR_SHOW, 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
